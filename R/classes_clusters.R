33f723fc772bf9aa920ec7acfb84b372d669452c
Lilly Schwietzer <lillyschwietzer@gmail.com>
2026-06-23 21:27:08 +0200

    default 2/3

---
+++ R/classes_clusters.R
@@ -0,0 +1,15 @@
+#' Species Classification
+#'
+#' An data set containing binary species occurrence data and coordinate information
+#'
+#' @format A data frame with 213 rows and 120 columns:
+#' \describe{
+#'   \item{species}{species name}
+#'   \item{Forest_Sp}{occurrence of forest species, binary (0/1)}
+#'   \item{Shrub_Sp}{occurrence of shrubland species, binary (0/1)}
+#'   \item{Grassland_Sp}{occurrence of grassland species, binary (0/1)}
+#'   \item{generalists_Sp}{occurrence of generalist species, binary (0/1)}
+#'   \item{other_specialists_Sp}{occurrence of other specialist species, binary (0/1)}
+#' }
+#' @source Original data from
+"classes_clusters"
